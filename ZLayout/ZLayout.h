2b25db4bc3cae476cb126a494402a547811537aa
Igor Zarubin <fanticqq@gmail.com>
2017-05-10 14:53:55 +0300

    Initial Commit

---
+++ ZLayout/ZLayout.h
@@ -0,0 +1,19 @@
+//
+//  ZLayout.h
+//  ZLayout
+//
+//  Created by Igor Zarubin on 10/05/2017.
+//  Copyright © 2017 Igor Zarubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZLayout.
+FOUNDATION_EXPORT double ZLayoutVersionNumber;
+
+//! Project version string for ZLayout.
+FOUNDATION_EXPORT const unsigned char ZLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLayout/PublicHeader.h>
+
+
