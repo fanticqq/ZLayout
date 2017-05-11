ad7f2e7e4c15b2c4fdf85f2e85b4fbd9718a3832
Igor Zarubin <fanticqq@gmail.com>
2017-05-11 23:14:50 +0300

    Fixed auto measuring of views.

--- ZLayout/ZLayoutPlaceable.swift
+++ ZLayout/ZLayoutPlaceable.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-public protocol ZLayoutPlaceable: ZLayoutable {
+public protocol ZLayoutAlignable: ZLayoutable {
 }
 
-extension ZLayoutPlaceable {
+extension ZLayoutAlignable {
     
 }
